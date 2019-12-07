5e539aba784be1ee228eeec0c3e462329252d3e1
manuel <manuel.c.a.075@gmail.com>
2019-12-07 12:46:46 -0400

    primera migracion

--- apps/account/models.py
+++ apps/account/models.py
@@ -59,6 +59,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
     is_transportista = models.BooleanField(default=False)
     is_soporte = models.BooleanField(default=False)
 
