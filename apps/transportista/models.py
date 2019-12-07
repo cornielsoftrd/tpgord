020b2300419f1c6fd91f2af48b1afc0eaa329a8f
manuel <manuel.c.a.075@gmail.com>
2019-12-07 10:11:58 -0400

    cambiando relacion de vendor y transportista, un vendor tiene muchos transportistas

--- apps/transportista/models.py
+++ apps/transportista/models.py
@@ -11,7 +11,8 @@ class Transportista(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     ruta = models.ManyToManyField(Ruta)
-    vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE, null=True, blank=True)
+    vendor = models.ManyToManyField(Vendor,  null=True, blank=True)
 
     def __str__(self):
         return "%s %s" % (self.nombre, self.apellido)
+ 
