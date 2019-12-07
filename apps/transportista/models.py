0731b451d49e6851f2101ec272a6c8de32291849
manuel <manuel.c.a.075@gmail.com>
2019-12-07 12:03:54 -0400

    cambiando relacion de vendor y transportista de uno a mucho con ForeignKey, un vendor tiene muchos transportistas

--- apps/transportista/models.py
+++ apps/transportista/models.py
@@ -11,7 +11,7 @@ class Transportista(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     ruta = models.ManyToManyField(Ruta)
-    vendor = models.ManyToManyField(Vendor)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return "%s %s" % (self.nombre, self.apellido)
