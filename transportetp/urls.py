8b119b47e197d2b34e62cb9bc384f25c256a1c7f
Manuel Corniel <manuelthewise@gmail.com>
2019-12-18 04:48:41 -0400

    recuperador de contraseñas, cambiar contraseña

--- transportetp/urls.py
+++ transportetp/urls.py
@@ -110,10 +110,10 @@ urlpatterns = [
     # manejo de contraseñas
     path("accounts/", include("django.contrib.auth.urls")),  
 
-    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
+    path('password_change/done/', login_required(PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html')), 
         name='password_change_done'),
 
-    path('password_change/',PasswordChangeView.as_view(template_name='registration/password_change.html'), 
+    path('password_change/',login_required(PasswordChangeView.as_view(template_name='registration/password_change.html')), 
         name='password_change'),
 
     # new
