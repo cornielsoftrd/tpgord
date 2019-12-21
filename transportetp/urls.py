524d823af37b710c2fc277dd9f30c50ac3707e75
Manuel <manuel.c.a.075@gmail.com>
2019-12-21 11:31:46 +0800

    500

--- transportetp/urls.py
+++ transportetp/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import handler404
+from django.conf.urls import handler404, handler500
 from django.urls import path, include
 
 from django.contrib.auth.views import (
@@ -32,7 +32,8 @@ from django.conf.urls.static import static
 
 from apps.home.views import (
     home_View,
-    error_404
+    error_404,
+    error_500,
     
     )
 
@@ -219,3 +220,4 @@ urlpatterns = [
     ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = error_404
+handler500 = error_500
